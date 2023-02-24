fc4cb30cfd99420a0aac1738255a4561e681a27a
Ahmed Anwer <ahmedanwer0094@gmail.com>
2023-02-24 08:59:08 +0530

    User Email Verification

--- app/src/main/java/com/example/futurebank/LoginActivity.java
+++ app/src/main/java/com/example/futurebank/LoginActivity.java
@@ -7,23 +7,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import es.dmoral.toasty.Toasty;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String email=null;
+    String password=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
-
+    public void verify(){
+        Toasty.info(this, "Please verify your account, Verification link send to "+email, Toast.LENGTH_LONG, true).show();
+    }
+    public void error(String message){
+        Toasty.error(this, message, Toast.LENGTH_LONG, true).show();
+    }
 
     public void signup(View v){
         Intent i = new Intent(this, SignupActivity.class);
@@ -31,25 +46,28 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void firebase(View v){
-        Intent i = new Intent(this,HomeActivity.class);
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        String email= ((EditText)findViewById(R.id.emailLog)).getText().toString();
-        String password= ((EditText)findViewById(R.id.passwordLog)).getText().toString();
-
+        email =((EditText)findViewById(R.id.emailLog)).getText().toString();
+        password= ((EditText)findViewById(R.id.passwordLog)).getText().toString();
+        Intent i = new Intent(this,HomeActivity.class);
         auth.signInWithEmailAndPassword(email,password)
-            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        startActivity(i);
-                    } else {
-                        // Account creation failed
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = auth.getCurrentUser();
+                            if(user.isEmailVerified()){
+
+                                startActivity(i);
+                            }else {
+                                verify();
+                            }
+                        } else {
+                            error(task.getException().getMessage());
+                        }
                     }
-                }
-            });
-
+                });
     }
-
 
 
 }
--- app/src/main/java/com/example/futurebank/SignupActivity.java
+++ app/src/main/java/com/example/futurebank/SignupActivity.java
@@ -1,53 +1,88 @@
 package com.example.futurebank;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+
+import es.dmoral.toasty.Toasty;
 
 public class SignupActivity extends AppCompatActivity {
 
+    String email;
+    String password;
+    String confirmPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
 
+    public void verify(){
+        Toasty.success(this, "Email verification link send to "+email, Toast.LENGTH_LONG, true).show();
+    }
+    public void error(String message){
+
+        Toasty.error(this, message, Toast.LENGTH_LONG, true).show();
+    }
     public void login(View v){
         Intent i = new Intent(this,LoginActivity.class);
         startActivity(i);
     }
     public void firebase(View v){
-        Intent i = new Intent(this,HomeActivity.class);
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        String email= ((EditText)findViewById(R.id.emailSign)).getText().toString();
-        String password= ((EditText)findViewById(R.id.passwordSign)).getText().toString();
-        String confirmPass= ((EditText)findViewById(R.id.confirmSign)).getText().toString();
+        Intent i = new Intent(this,LoginActivity.class);
+        email= ((EditText)findViewById(R.id.emailSign)).getText().toString();
+        password= ((EditText)findViewById(R.id.passwordSign)).getText().toString();
+        confirmPass= ((EditText)findViewById(R.id.confirmSign)).getText().toString();
 
         if(password.equals(confirmPass)){
+            FirebaseAuth auth = FirebaseAuth.getInstance();
             auth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            startActivity(i);
+                            FirebaseUser user =auth.getCurrentUser();
+                            if(user !=null){
+                                user.sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                verify();
+                                                startActivity(i);
+                                            }else {
+                                                error(task.getException().getMessage());
+                                            }
+                                        }
+                                    });
+                            }
                         } else {
-                            // Account creation failed
+                            error(task.getException().getMessage());
                         }
                     }
                 });
-
         }else {
+            Toasty.error(this, "Confirm password doesn't match", Toast.LENGTH_SHORT, true).show();
 
         }
+
+
+
 
 
     }
